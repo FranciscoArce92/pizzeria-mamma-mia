f50d99311caec27e3f33dfde3f551d9c344dab52
FranciscoArce92 <franciscojavier.arce1992@gmail.com>
2025-01-13 11:50:37 -0300

    finish navbar section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@
 
 function Header() {
   return (
-    <div className="header">
-        <h1>¡Pizzería Mamma Mía!</h1>
-        <p>Tenemos las mejores pizzas que podrás encontrar!</p>
+    <div className="header text-white p-4">
+        <h1 className="fs-3">¡Pizzería Mamma Mía!</h1>
+        <p className="fs-5">Tenemos las mejores pizzas que podrás encontrar!</p>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,101 @@
 const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-                    <form className="d-flex" role="search">
-                        <input type="text" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
-            </div>
-        </nav>
-        
-    )
-}
+    const total = 25000;
+    const token = false;
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  <img
+                    src="./src/assets/img/pizza.png"
+                    className="img-fluid rounded-top"
+                    width={"20px"}
+                    alt=""
+                  />
+                  Home
+                </a>
+              </li>
+              
+              {/* Renderización condicional según el estado de token */}
+              {token ? (
+                <>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      <img
+                        src="./src/assets/img/icons8-user-24.png"
+                        width={"20px"}
+                        alt=""
+                      />
+                      Profile
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      <img
+                        src="./src/assets/img/icons8-salida-30.png"
+                        width={"20px"}
+                        alt=""
+                      />
+                      Logout
+                    </a>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      <img
+                        src="./src/assets/img/icons8-accede-redondeado-derecho-24.png"
+                        width={"20px"}
+                        alt=""
+                      />
+                      Login
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      <img
+                        src="./src/assets/img/icons8-añadir-usuario-masculino-30.png"
+                        width={"20px"}
+                        alt=""
+                      />
+                      Register
+                    </a>
+                  </li>
+                </>
+              )}
+
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  <img
+                    src="./src/assets/img/icons8-carrito-de-compras-30.png"
+                    width={"20px"}
+                    alt=""
+                  />
+                  Total: ${total.toLocaleString()}
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
 
 export default Navbar;
