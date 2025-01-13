25b9f7b4742da99770d5fbe2ea587ce3488ec1e3
FranciscoArce92 <franciscojavier.arce1992@gmail.com>
2025-01-13 14:38:29 -0300

    finish proyect

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,9 @@
-// import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="container-fluid bg-dark text-light p-2 mt-2 d-flex justify-content-center align-items-center" style={{ height: '60px' }}>
+      <p className="mb-0">@ 2021 - Pizerría Mamma Mía! - Todos los derechos reservados.</p>
+    </div>
   )
 }
 
